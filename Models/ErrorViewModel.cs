d7c794a0d52824587565350c6003e34d1fcb34b2
Admin <Admin@DESKTOP-AAGAB07>
2021-10-06 08:46:01 +0700

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NguyenMinhTrong853.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
